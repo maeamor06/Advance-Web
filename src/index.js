5e43847f110fc9fbea0526b827b3cd6ae7c6ac00
maeamor06 <mae.ocampos@lsu.edu.ph>
2022-02-24 14:52:56 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello my name is <br> Mae Amor D. Ocampos!</h1>
+<div>
+  I like coding even if coding doesn't like me!
+</div>
+`;
